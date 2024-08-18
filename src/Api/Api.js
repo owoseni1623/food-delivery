151f522e9aff0839fb30447d0240d6c5f370bfa9
god_is_able <owoseni0816@gmail.com>
2024-08-18 04:32:57 -0700

    login

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,30 +1,30 @@
-// src/api.js
-import axios from 'axios';
+// // src/api.js
+// import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000',
-  headers: {
-    'Content-Type': 'application/json',
-    // Add any other headers your API requires
-  },
-});
+// const api = axios.create({
+//   baseURL: 'http://localhost:3000',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     // Add any other headers your API requires
+//   },
+// });
 
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        await axios.post('http://localhost:3000/api/user/refresh-token', {}, { withCredentials: true });
-        return api(originalRequest);
-      } catch (refreshError) {
-        window.location.href = '/login';
-        return Promise.reject(refreshError);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
+//       try {
+//         await axios.post('http://localhost:3000/api/user/refresh-token', {}, { withCredentials: true });
+//         return api(originalRequest);
+//       } catch (refreshError) {
+//         window.location.href = '/login';
+//         return Promise.reject(refreshError);
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
-export default api;
+// export default api;
