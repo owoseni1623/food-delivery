eb51072cfff38069c94886e48b6ef0c2053fdde6
god_is_able <owoseni0816@gmail.com>
2024-08-29 18:04:12 -0700

    header

--- src/Context/EcomContext.jsx
+++ src/Context/EcomContext.jsx
@@ -226,8 +226,7 @@ export const EcomProvider = ({ children }) => {
     try {
       const localCart = JSON.parse(localStorage.getItem('cart')) || [];
       if (localCart.length > 0) {
-        console.log('Merging carts...');
-        const response = await axiosInstance.post(`${apiUrl}/api/users/merge-cart`, { localCart });
+        const response = await axiosInstance.post(`${apiUrl}/api/cart/merge`, { localCart });
         if (response.data.success) {
           setCart(response.data.cartData);
           localStorage.removeItem('cart');
